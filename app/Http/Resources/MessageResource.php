4c7a155b8ea2588c8587409cf7fe2e3243cf0498
Billy <billyfranklim@gmail.com>
2024-03-30 22:11:14 -0300

    ✨ feat(resources): add LoginResource, MessageResource, TokenResource, and UserResource classes

---
+++ app/Http/Resources/MessageResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class MessageResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'message' => $this['message'],
+            'data' => $this['data'] ?? null,
+            'status' => $this['status'] ?? 'success',
+        ];
+    }
+}
